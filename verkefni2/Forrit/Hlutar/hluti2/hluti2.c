153a7a0fd6287312753b51e8f1319391581e75d9
KyplerEspiritu <kypleres312@nemi.tskoli.is>
2018-11-13 08:27:39 +0000

    Update hluti2.c

--- verkefni2/Forrit/Hlutar/hluti2/hluti2.c
+++ verkefni2/Forrit/Hlutar/hluti2/hluti2.c
@@ -10,25 +10,7 @@
 |*                                                                                                    *|
 |*  This program instructs your robot to move forward at full power for three seconds.  There is a    *|
 |*  two second pause at the beginning of the program.                                                 *|
-|*                                                                                                    *|
-|*                                        ROBOT CONFIGURATION                                         *|
-|*    NOTES:                                                                                          *|
-|*    1)  Reversing 'rightMotor' (port 2) in the "Motors and Sensors Setup" is needed with the        *|
-|*        "Squarebot" mode, but may not be needed for all robot configurations.                       *|
-|*    2)  Power levels that can be assigned to a motor port range from -127 (full reverse) to         *|
-|*        127 (full forward).                                                                         *|
-|*                                                                                                    *|
-|*    MOTORS & SENSORS:                                                                               *|
-|*    [I/O Port]          [Name]              [Type]                [Description]                     *|
-|*    Motor Port 2        rightMotor          VEX 3-wire module     Right side motor                  *|
-|*    Motor Port 3        leftMotor           VEX 3-wire module     Left side motor                   *|
 \*-----------------------------------------------------------------------------------------------4246-*/
-// Thvermal = 10,3
-
-// ummal deks 33cm
-// 33 = 100 %
-// 50 = 151,5%
-// 360 * 1,515 = 545,4
 
 #include "../../../../program_connector/Functions.c"
 
